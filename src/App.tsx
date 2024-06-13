e77b138d6e6e4cc9fb593a6159a12fd2355358c1
Eckry <112786854+Eckry@users.noreply.github.com>
2024-06-12 18:05:32 -0600

    add basic funcionality to frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <main>Hello!</main>;
+  const [url, setUrl] = useState("");
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setUrl(event.target.value);
+  }
+
+  async function handleClick() {
+    const body = { url: url, name: "back" };
+    const result = await fetch("http://localhost:3000/api/addurl", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    });
+    const data = await result.json();
+    console.log(data);
+  }
+
+  return (
+    <main>
+      <input type="text" onChange={handleChange} value={url} />
+      <input type="text" />
+      <button onClick={handleClick}>SUBMIT</button>
+    </main>
+  );
 }
 
 export default App;
