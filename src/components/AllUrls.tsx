b7ff4cecd480c7daf110c9933b403df86e96ab73
Eckry <112786854+Eckry@users.noreply.github.com>
2024-06-15 18:33:22 -0600

    Add AllUrls component

---
+++ src/components/AllUrls.tsx
@@ -0,0 +1,21 @@
+import { type Url } from "../types";
+import "./styles/AllUrls.css";
+
+interface Props {
+  urls: Url[];
+}
+
+export default function AllUrls({ urls }: Props) {
+  return (
+    <section className="urls-section">
+      {urls.map(({ shortUrl, realUrl }) => {
+        return (
+          <a href={realUrl} className="url-container">
+            <p>{shortUrl}</p>
+            <p>{realUrl}</p>
+          </a>
+        );
+      })}
+    </section>
+  );
+}
