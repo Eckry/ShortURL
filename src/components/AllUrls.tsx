ae0f45e4f6bcda0d315d7e89963c4026d337f445
Eckry <112786854+Eckry@users.noreply.github.com>
2024-06-15 19:58:28 -0600

    add deleteUrl funcionality

--- src/components/AllUrls.tsx
+++ src/components/AllUrls.tsx
@@ -1,13 +1,31 @@
 import { useEffect, useState } from "react";
-import { type Url } from "../types";
+import { type deleteApiResponse, type Url } from "../types";
 import "./styles/AllUrls.css";
 
 interface Props {
   urls: Url[];
+  deleteUrl: (urlToDelete: string) => void;
 }
 
-export default function AllUrls({ urls }: Props) {
+export default function AllUrls({ urls, deleteUrl }: Props) {
   const [urlsOwned, setUrlsOwned] = useState<Record<string, string>>({});
+
+  async function deleteUrlApi(shortUrl: string) {
+    try {
+      const res = await fetch("http://localhost:3000/api/deleteurl", {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ url: shortUrl }),
+      });
+
+      if (!res.ok) throw new Error("Something ocurred");
+
+      const data = (await res.json()) as deleteApiResponse;
+      deleteUrl(shortUrl);
+    } catch (err) {
+      console.error(err);
+    }
+  }
 
   useEffect(() => {
     const hasUrlsOwned = localStorage.getItem("urlsOwned");
@@ -20,11 +38,20 @@ export default function AllUrls({ urls }: Props) {
     <section className="urls-section">
       {urls.map(({ shortUrl, realUrl }) => {
         return (
-          <a key={shortUrl} href={realUrl} className="url-container">
-            <p>{shortUrl}</p>
-            <p className="realurl">{realUrl}</p>
-            {!!urlsOwned[shortUrl] && <button>HELLO</button>}
-          </a>
+          <div className="relative go-up">
+            <a key={shortUrl} href={realUrl} className="url-container">
+              <p>{shortUrl}</p>
+              <p className="realurl">{realUrl}</p>
+            </a>
+            {!!urlsOwned[shortUrl] && (
+              <button
+                className="delete-button"
+                onClick={() => deleteUrlApi(shortUrl)}
+              >
+                X
+              </button>
+            )}
+          </div>
         );
       })}
     </section>
