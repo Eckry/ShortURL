f9c55bd43e8bb0335ece5addbaf6b86bdfd17096
Eckry <112786854+Eckry@users.noreply.github.com>
2024-06-17 17:45:35 -0600

    create updateClicks service

---
+++ src/services/updateClicks.ts
@@ -0,0 +1,19 @@
+export default async function updateClicks(
+  shortUrl: string
+): Promise<[Error?, boolean?]> {
+  try {
+    const res = await fetch("http://localhost:3000/api/updateclicks", {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ shortUrl }),
+    });
+
+    if (!res.ok) return [new Error("Error updating the clicks")];
+
+    return [undefined, true];
+  } catch (err) {
+    if (err instanceof Error) return [err];
+  }
+
+  return [new Error("Unknown error")];
+}
