530105190af2f4e4054c65777818f4e41053610b
Eckry <112786854+Eckry@users.noreply.github.com>
2024-06-19 18:20:35 -0600

    add ArrowIcon

--- src/icons.tsx
+++ src/icons.tsx
@@ -16,3 +16,23 @@ export const CloseIcon = () => {
     </svg>
   );
 };
+
+export const ArrowIcon = () => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="#e19e6a"
+      fill="none"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M12 5l0 14" />
+      <path d="M16 15l-4 4" />
+      <path d="M8 15l4 4" />
+    </svg>
+  );
+};
