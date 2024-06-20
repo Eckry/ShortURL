9f84c98cd9db4d576ad8c946908a894ce999f550
Eckry <112786854+Eckry@users.noreply.github.com>
2024-06-19 18:44:27 -0600

    add tyles to Message component

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,21 +1,32 @@
 import "./styles/Message.css";
 import CopyToClipboard from "react-copy-to-clipboard";
 import { addApiResponse } from "../types.d";
+import { ArrowIcon, CopyIcon } from "../icons";
+import { toast } from "sonner";
 
 interface Props {
   lastUrl: addApiResponse;
 }
 
 export default function Message({ lastUrl }: Props) {
+  function handleClick() {
+    toast.success("Link copied!");
+  }
+
   return (
-    <section>
-      <h2>{lastUrl.message}!</h2>
-      <p>{lastUrl.shortUrl} leads you to</p>
-      <CopyToClipboard text={lastUrl.realUrl}>
-        <p>{lastUrl.realUrl}</p>
+    <section className="message-section">
+      <h2 className="message-title">{lastUrl.message}!</h2>
+      <p className="message-realurl">{lastUrl.realUrl}</p>
+      <ArrowIcon />
+      <CopyToClipboard text={`${window.location.origin}/${lastUrl.shortUrl}`}>
+        <p onClick={handleClick} className="message-shorturl">
+          {window.location.origin}/{lastUrl.shortUrl}
+        </p>
       </CopyToClipboard>
-      <CopyToClipboard text={lastUrl.realUrl}>
-        <button>Copy</button>
+      <CopyToClipboard text={`${window.location.origin}/${lastUrl.shortUrl}`}>
+        <button onClick={handleClick} className="copy">
+          <CopyIcon />
+        </button>
       </CopyToClipboard>
     </section>
   );
